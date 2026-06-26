3e6be8a8cc8e983a5149a8e81bd5ade230d47314
adikhan4 <adik62056@gmail.com>
2026-06-26 14:49:25 +0530

    Add files via upload

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,10 @@
+//program to add two number
+#include<stdio.h>
+void main()
+{
+	float a,b,sum;
+	printf("Enter the value of a and b:");
+	scanf("%f%f",&a,&b);
+	sum= a+b;
+    printf("sum = %f",sum);
+}
