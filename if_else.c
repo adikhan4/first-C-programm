a68004c7842d5c5c699b733b2877812b5b484578
adikhan4 <adik62056@gmail.com>
2026-06-28 22:31:03 +0530

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,30 @@
+//finding greater number using if else
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter three number:");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			printf("A is greater");
+		}
+		else
+		{
+			printf("c is greater");
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			printf("b is greater");
+		}
+		else
+		{
+			printf("c is greater");
+		}
+    }
+}
