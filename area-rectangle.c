52e9eaad6bf781cd1106057e4a0372b39e3a74ab
adikhan4 <adik62056@gmail.com>
2026-06-29 23:57:26 +0530

    Add files via upload

---
+++ area-rectangle.c
@@ -0,0 +1,11 @@
+//area of rectangle 
+#include<stdio.h>
+void main()
+{
+	float w, l, ar;
+	printf("Enter the lenght and width of rectangle:");
+	scanf("%f%f", &l,&w);
+	
+	ar=l*w;
+	printf("The area of the rectangle is:%f", ar);
+}
