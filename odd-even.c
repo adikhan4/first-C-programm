abfbb4c441974a8a652361e7dbace988f141f213
adikhan4 <adik62056@gmail.com>
2026-07-01 23:52:39 +0530

    Add files via upload

---
+++ odd-even.c
@@ -0,0 +1,14 @@
+//odd even using if else
+#include<stdio.h>
+voi`d main()
+{
+	int a;
+	printf("Enter a number:");
+	scanf("%d", &a);
+	if(a%2==0)
+	printf("The number is even number");
+	else if(a%2!=0)
+	printf("The number is a odd number");
+	else
+	printf("The number is a non divisible");
+}
