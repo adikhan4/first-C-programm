a4da96cf022601d1c930e80ac33eb0fc3be1de2b
adikhan4 <adik62056@gmail.com>
2026-07-06 23:05:34 +0530

    Add files via upload

---
+++ sum-of-series.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,a=-1,b=1,sum;
+	printf("Enter the ending of the series");
+	scanf("%d",&n);
+	for(i=1;i<=n ;i++)
+	{
+		sum=a+b;
+		printf("%d\t",sum);
+		a=b;
+		b=sum;
+	}
+}
